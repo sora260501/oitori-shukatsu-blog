d3d7fd52ef58d2fab5e2731401523e6d11ae38e6
田畑裕子 <tabatahiroko@soraMacBook-Air.local>
2026-05-01 09:32:55 +0900

    Add SEO settings and 3 new articles

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -19,6 +19,16 @@ export async function generateMetadata({
   return {
     title: post.title,
     description: post.description,
+    openGraph: {
+      url: `https://oitori-shukatsu-blog.vercel.app/blog/${slug}`,
+      images: [
+        {
+          url: "/og-image.png",
+          width: 1200,
+          height: 630,
+        },
+      ],
+    },
   };
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,14 @@ export const metadata: Metadata = {
     type: "website",
     locale: "ja_JP",
     siteName: "おひとり様の終活ノート",
+    url: "https://oitori-shukatsu-blog.vercel.app",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
   },
 };
 
